0fdbf800f8ca16ae69dcd383ef4281e30a34dfaf
Nana <georgesdemb123@gmail.com>
2024-02-15 15:43:41 +0530

    ready to move to backend now

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,12 +1,23 @@
 import { StyleSheet, Image, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import {Ionicons} from  '@expo/vector-icons';
+import React, {useState} from 'react'
+import {Ionicons, SimpleLineIcons, MaterialCommunityIcons, Fontisto} from  '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styles from './productDetails.style'
-import { COLORS } from '../constants';
+import { COLORS, SIZES } from '../constants';
 
 
 const ProductDetails = ({navigation}) => {
+    const [count, setCount] = useState(1)
+
+    const increment = ()=>{
+        setCount(count+1)
+    }
+    const decrement = ()=>{
+        if(count>1){
+            setCount(count-1)
+        }
+    }
+
   return (
     <View style={styles.container}>
         <View style={styles.upperRow}>
@@ -24,9 +35,70 @@ const ProductDetails = ({navigation}) => {
         />
 
         <View style={styles.details}>
-            <View style={styles.titleRow}></View>
-        </View>
+            <View style={styles.titleRow}>
+                <Text style={styles.title}>Product</Text>
+                <View style={styles.priceWrapper}>
+                    <Text style={styles.price}>$ 660.88</Text>
+                </View>
+            </View>
+            <View style={styles.ratingRow}>
+                <View style={styles.rating}>
+                    {[1,2,3,4,5].map((index)=>(
+                        <Ionicons
+                          key={index}
+                          name="star"
+                          size={24}
+                          color="gold"
+                        />
+                    ))}
+                    <Text style={styles.ratingText}> (4.9)</Text>
+                </View>
+                <View style={styles.rating}>
+                    <TouchableOpacity onPress={()=> increment()}>
+                        <SimpleLineIcons
+                            name='plus' size={20}
+                        />
+                    </TouchableOpacity>
+                    <Text style={styles.ratingText}>{count}</Text>
+                    <TouchableOpacity onPress={()=> decrement()}>
+                        <SimpleLineIcons
+                            name='minus' size={20}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.descriptionWrapper}>
+                <Text style={styles.description}>Description</Text>
+                <Text style={styles.descText}>Lorem ipsum dolor sit amet. Quo earum quam qui officiis maiores sit nostrum voluptate et sunt facilis est quam fugiat non expedita quia. Est corporis sunt rem laborum dolorem et dignissimos blanditiis. Aut commodi voluptatum qui quasi culpa et vero impedit ut enim beatae.
+Eum totam consequatur qui fugit excepturi et repellat inventore et provident reprehenderit. Ut debitis molestias id labore nobis ea voluptatum totam hic amet eaque est eius assumenda non saepe eaque.
+At quod consequatur ea fugit porro sed perspiciatis officiis qui numquam quasi aut illo atque. Non galisum repellat non obcaecati doloribus aut dolor fugiat sit distinctio tenetur.
+                 </Text>
+            </View>
+            <View style={{marginBottom: SIZES.small}}>
+                <View style={styles.location}>
+                   <View style={{flexDirection: 'row'}}>
+                      <Ionicons name="location-outline" size={20}/> 
+                      <Text> Bamako </Text>
+                   </View>
+                   <View style={{flexDirection: 'row'}}>
+                      <MaterialCommunityIcons name="truck-delivery-outline" size={20}/> 
+                      <Text> Free Delivery </Text>
+                   </View>
+                </View>
+            </View>
+
+            <View style={styles.cartRow}>
+                <TouchableOpacity onPress={()=>{}} style={styles.cartBtn}>
+                    <Text style={styles.cartTitle}>BUY NOW</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>{}} style={styles.addCart}>
+                     <Fontisto name="shopping-bag" size={22} color={COLORS.lightWhite}/>
+                </TouchableOpacity>
+                
+            </View>
         
+        </View>
     </View>
   )
 }
--- screens/productDetails.style.js
+++ screens/productDetails.style.js
@@ -1,9 +1,11 @@
 import { StyleSheet } from "react-native";
 import {COLORS, SIZES} from "../constants";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+        backgroundColor: COLORS.lightWhite,
     },
     upperRow:{
         marginHorizontal: 20,
@@ -20,8 +22,109 @@ const styles = StyleSheet.create({
         resizeMode: "cover"
     },
     details: {
-        marginTop: -SIZES.small,
+        marginTop: -SIZES.large,
+        backgroundColor: COLORS.lightWhite,
+        width: SIZES.width,
+        borderTopLeftRadius: SIZES.medium,
+        borderTopRightRadius: SIZES.medium,
+    },
+    cartRow:{
+        paddingBottom: SIZES.small,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: "Center",
+        width: SIZES.width,  
+    }, 
+    cartBtn:{
+        width: SIZES.width*0.7,
+        backgroundColor: COLORS.black,
+        padding: SIZES.small/2,
+        borderRadius: SIZES.large,
+        marginLeft: 12,  
+        //marginTop: 6,
+    }, 
+    titleRow:{
+        marginHorizontal: 20,
+        paddingBottom: SIZES.small,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: "Center",
+        width: SIZES.width -44,
+        top: 20,
+    }, 
+    ratingRow:{
+        paddingBottom: SIZES.small,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: "Center",
+        width: SIZES.width -10,
+        top: 5,
+    },
+    rating:{
+        top: SIZES.large,
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        marginHorizontal: SIZES.large,
+    },
+    ratingText:{
+        color: COLORS.gray,
+        fontFamily:"mediun",
+        paddingHorizontal: 5,
+    },
+    descriptionWrapper:{
+        marginTop: SIZES.large+2,
+        marginHorizontal: SIZES.large,
+    },
+    description:{
+        fontFamily: "medium",
+        fontSize: SIZES.large -2,
+    },
+    descText:{
+        fontFamily: "regular",
+        fontSize: SIZES.small,
+        textAlign: "justify",
+        marginBottom: SIZES.small,
+    },
+    location:{
+        flexDirection:'row',
+        justifyContent:"space-between",
+        alignItems:"center",
+        backgroundColor:COLORS.secondary,
+        marginHorizontal:12,
+        padding: 5,
+        borderRadius: SIZES.large,
+    },
+    cartTitle:{
+        marginLeft: SIZES.small,
+        fontFamily: "semibold",
+        fontSize: SIZES.mediun,
+        color: COLORS.lightWhite,
+    },
+    title:{
+        fontFamily: "bold",
+        fontSize: SIZES.large,
+    },
+    price:{
+        paddingHorizontal: 10,
+        fontFamily: "bold",
+        fontSize: SIZES.large,
+    },
+    priceWrapper:{
+        backgroundColor: COLORS.secondary,
+        borderRadius: SIZES.large,
+    },
+    addCart:{
+        width: 37,
+        height: 37,
+        borderRadius: 50,
+        backgroundColor: COLORS.black,
+        margin: SIZES.small,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: -1,
     }
+
 })
 
 export default styles
