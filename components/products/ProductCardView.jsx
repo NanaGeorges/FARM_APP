56bff447c1c316cba7f967acb68e55e6239b5488
Nana <georgesdemb123@gmail.com>
2024-02-21 23:20:31 +0530

    search title and navigation

--- components/products/ProductCardView.jsx
+++ components/products/ProductCardView.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image, Touchable } from 'react-native'
+import { View, Text, TouchableOpacity, Image} from 'react-native'
 import React from 'react'
 import styles from './productCardView.style'
 import {Ionicons} from "@expo/vector-icons"; 
--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,11 +1,27 @@
-import { StyleSheet, TouchableOpacity, TextInput, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, TouchableOpacity, TextInput, View, Text} from 'react-native'
+import React, {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styles from './search.style'
 import {COLORS, SIZES } from "../constants";
 import {Feather, Ionicons} from '@expo/vector-icons';
+import axios from 'axios';
+//import { FlatList } from 'react-native-gesture-handler';
 
 const Search = () => {
+    const [searchKey, setSearchKey] = useState("");
+    const [searchResults, setSearchResuts] = useState([]);
+    console.log(searchResults);
+
+    //http://localhost:3000/api/products/search/${searchKey}
+    const handleSearch = async() =>{
+        try {
+            const response = await axios.get(`http://192.168.1.4:3000/api/products/search/${searchKey}`);
+            setSearchResuts(response.data)
+        } catch (error) {
+            console.log("Failed to get products", error);
+        }
+    }
+
   return (
     <SafeAreaView>
         <View style= {styles.searchContainer}>
@@ -15,13 +31,13 @@ const Search = () => {
             <View style={styles.searchWrapper}>
                 <TextInput
                     style={styles.searchInput}
-                    value=""
-                    onPressIn={()=>{}}
+                    value={searchKey}
+                    onChangeText={setSearchKey}
                     placeholder='What are you looking for'
                 />
             </View>
             <View>
-                <TouchableOpacity style={styles.searchBtn}>
+                <TouchableOpacity style={styles.searchBtn} onPress={()=>handleSearch()}>
                     <Feather name="search" 
                       size={24}
                       color={COLORS.offwhite}
@@ -29,6 +45,20 @@ const Search = () => {
                 </TouchableOpacity>
             </View>
         </View>
+        {searchResults.length === 0 ? (
+            <View style= {{flex: 1}} >
+                <Image
+                    source={require('../assets/images/Pose23.png')}
+                    style={styles.searchImage}
+                />
+            </View>
+        ):(
+            <FlatList
+                data={searchResults}
+                keyExtractor= {item => item._id}
+                renderItem ={({item}) => (<Text> {item.title} </Text>)}
+            />
+        )}
     </SafeAreaView>
   )
 }
